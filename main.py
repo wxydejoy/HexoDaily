1a60d80cd40bd24d6ac24d5456d60aee87a65d13
wxydejoy <1798923421@qq.com>
2021-11-09 12:59:35 +0800

    文件遍历完成，大体框架完成

--- main.py
+++ main.py
@@ -2,26 +2,72 @@ import os
 import subprocess as sp
 import sys
 import re
-
+import string
 
 
 
 def cmd(command,cwdd): # cmd
+    command = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe " + command
     p = sp.run(command, shell=True,
                  cwd=cwdd,
                  encoding="UTF-8",stdout=sp.PIPE,stderr=sp.PIPE)
 
     return list(p.stdout)
 
-def getnpmversion(bag): # 获取npm包的版本号 并加一
-    str = cmd("C://WINDOWS//system32//WindowsPowerShell//v1.0//powershell.exe npm view "+bag+" version", "D://Windows//wd//GitHub//blog")
-    str[len(str) - 2] = int(str[len(str) - 2]) + 1
+def cmd(command): # cmd
+    command = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe " + command
+    p = sp.run(command, shell=True,
+                 encoding="UTF-8",stdout=sp.PIPE,stderr=sp.PIPE)
 
-    return str[0:len(str)-1]
+    return list(p.stdout)
+
+
+def getnpmversion(bag): # 获取npm包的版本号 并加一
+
+    strr = cmd("npm view "+bag+" version")
+    strr[-2] = int(strr[-2]) + 1
+    return ''.join(list(map(str, strr)))
+
+
+
+def get_file_path(root_path,file_list,dir_list): #遍历文件
+    #获取该目录下所有的文件名称和目录名称
+    dir_or_files = os.listdir(root_path)
+    for dir_file in dir_or_files:
+        #获取目录或者文件的路径
+        dir_file_path = os.path.join(root_path,dir_file)
+        #判断该路径为文件还是路径
+        if os.path.isdir(dir_file_path):
+            dir_list.append(dir_file_path)
+            #递归获取所有文件和目录的路径
+            get_file_path(dir_file_path,file_list,dir_list)
+        else:
+            file_list.append(dir_file_path)
+
+def mdreplace(root_path,git_name,git_re,domain,npm_name,version):
+    file_list = []
+    # 用来存放所有的目录路径
+    dir_list = []
+    get_file_path(root_path, file_list, dir_list)
+
+    print(file_list)
+
+
 
 if __name__ == '__main__':
+    ps = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe " #powershell
+    git_name = "wxydejoy"
+    git_re = "image"
+    domain = "unpkg.zhimg.com"
+    npm_name = "wxydejoy"
+    version = getnpmversion("wxydeimage")
+    print(version)
+    root_path = r"D:\Windows\wd\GitHub\blog\source\_posts"
+    # 用来存放所有的文件路径
+    mdreplace(root_path, git_name, git_re, domain, npm_name, version)
+    #print(file_list)
+    #print(dir_list)
 
-    print(getnpmversion("wxydeimage"))
 
 
 
