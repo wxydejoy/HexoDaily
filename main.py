3810e6894d2e272a7239a2c83b96141491102d87
wxydejoy <1798923421@qq.com>
2021-11-09 21:06:34 +0800

    文件遍历完成 大体框架完成 git 推送完成

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 import os
 import subprocess as sp
 import sys
-import re
+import time
 import string
 
 
 
+# 图床仓库自动更新版本并且推送到github
+# md文件自动查找替换域名
+# 自动推送博客
+
+
 def cmd(command,cwdd): # cmd
     command = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe " + command
-    p = sp.run(command, shell=True,
+    p = sp.Popen(command, shell=True,
                  cwd=cwdd,
                  encoding="UTF-8",stdout=sp.PIPE,stderr=sp.PIPE)
+    try:
+        outs, errs = p.communicate(timeout=150)
+    except p.TimeoutExpired:
+        p.kill()
+        outs, errs = p.communicate()
+    p.wait(100)
+    p.kill()
+    return list(outs)
 
-    return list(p.stdout)
-
-def cmd(command): # cmd
-    command = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe " + command
-    p = sp.run(command, shell=True,
-                 encoding="UTF-8",stdout=sp.PIPE,stderr=sp.PIPE)
-
-    return list(p.stdout)
-
+# def cmd(command): # cmd 其实是powershell
+#     command = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe " + command
+#     p = sp.Popen(command, shell=True,
+#                  encoding="UTF-8",stdout=sp.PIPE,stderr=sp.PIPE)
+#     try:
+#         outs, errs = p.communicate(timeout=150)
+#     except p.TimeoutExpired:
+#         p.kill()
+#         outs, errs = p.communicate()
+#     p.wait(100)
+#     p.kill()
+#     return list(outs)
+#
 
 def getnpmversion(bag): # 获取npm包的版本号 并加一
 
-    strr = cmd("npm view "+bag+" version")
+    strr = cmd("npm view "+bag+" version",None)
     strr[-2] = int(strr[-2]) + 1
     return ''.join(list(map(str, strr)))
 
@@ -44,34 +61,51 @@ def get_file_path(root_path,file_list,dir_list): #遍历文件
         else:
             file_list.append(dir_file_path)
 
-def urlreplace(root_path,git_name,git_re,domain,npm_name,version):
-    file_list = []
+def urlreplace(root_path,git_name,git_re,npm_name,version):
+    file_lists = []
     # 用来存放所有的目录路径
     dir_list = []
-    get_file_path(root_path, file_list, dir_list)
+    get_file_path(root_path, file_lists, dir_list)
     
-    for i in file_list:
-        fopen = open(filename, 'r+')
-        str = fopen.read()
-        str.replace(. xxx,xxx)
-    fopen.close()
-    
+    for file_list in file_lists:
+        file = open(file_list, 'r+',encoding="UTF-8")
+        s = file.read()
+        file.seek(0, 0)
+        s = s.replace("cdn.jsdelivr.net/gh/"+git_name+"/" + git_re, "unpkg.zhimg.com/"+npm_name + "@" + version)
+        file.write(s)
+    file.close()
+
+
+def updateimage(image_path):
+    cmd('git pull', image_path)
+    cmd('git add .', image_path)
+
+    cmd("git commit  -m ' npm 自动更新 '", image_path)
+
+    cmd("npm version patch ", image_path)
+
+    cmd("git push", image_path)
+
 
 
 if __name__ == '__main__':
-    ps = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe " #powershell
+
+    # 参数设置
+
     git_name = "wxydejoy"
     git_re = "image"
     domain = "unpkg.zhimg.com"
-    npm_name = "wxydejoy"
+    npm_name = "wxydeimage"
     version = getnpmversion("wxydeimage")
-    print(version)
-    root_path = r"D:\Windows\wd\GitHub\blog\source\_posts"
-    # 用来存放所有的文件路径
-    mdreplace(root_path, git_name, git_re, domain, npm_name, version)
-    #print(file_list)
-    #print(dir_list)
+    root_path = r"D:\Windows\zm\source\_posts" # 用来存放所有的文件路径
+    image_path = "D:\Windows\wd\GitHub\image"
 
+    updateimage(image_path)
+    # print(cmd("npm -version"))
+    # 替换
+    # urlreplace(root_path, git_name, git_re,  npm_name, version)
+    #
+    # updateimage(image_path)
 
 
 
